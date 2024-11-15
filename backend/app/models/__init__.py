4d0504e2457c8046ebedd2b4327bb53c57fcf2f5
rahul <53050874+rahuljassal@users.noreply.github.com>
2024-11-15 15:57:43 +0530

    REF : removed backend code

--- backend/app/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from app.db.base import Base
-from app.models.organization import Organization, OrganizationMember
-from app.models.service import Service
-from app.models.incident import Incident
-
-__all__ = ["Base", "Organization", "OrganizationMember", "Service", "Incident"]
